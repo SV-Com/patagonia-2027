b4a915130639634c5924fab5a7d944b8403ad4e4
SV-Com <svcomputacion@gmail.com>
2026-06-06 06:49:06 -0300

    PWA Patagonia 2027 - itinerario, finanzas y nafta

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'v1_patagonia_cache';
+const ASSETS = [
+  './',
+  './index.html',
+  'https://cdn.tailwindcss.com'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
